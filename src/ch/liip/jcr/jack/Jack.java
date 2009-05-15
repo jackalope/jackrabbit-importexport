704b0694a133f81dd73e664453a5c3aee00a7257
dbu <david.buchmann@liip.ch>
2009-05-15 16:06:32 +0200

    added davex support (with log4j.properties present, the logging exceptions went away) added support for command line overwriting all settings from jcr.properties

--- src/ch/liip/jcr/jack/Jack.java
+++ src/ch/liip/jcr/jack/Jack.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 import java.io.*;
 import javax.jcr.*;
 import org.apache.jackrabbit.core.TransientRepository;
-//import ch.liip.jcr.davex.DavexClient;
+import ch.liip.jcr.davex.DavexClient;
 
 /**
 Import or export the entire repository (system view).
@@ -13,14 +13,25 @@ Import or export the entire repository (system view).
 Export and import are done on path specified in src/jcr.properties as repository-base-xpath
 Import is done with behavior IMPORT_UUID_COLLISION_THROW, that is if there already is something in the repository and that happens to have the same UUID, the import will fail.
 Our primary goal is to replace the complete repository contents.
+
+The program can be controlled by a couple of parameters that are given in the form param=value.
+Parameters are specified after the import / export switch and the filename
+ * username, password: credentials for login
+ * repository-base-xpath: path to export to file / import file to
+ * workspace: workspace to export / import
+ * transport: davex to connect to a server, local to start an own jackrabbit server
+ * storage: davex transport only. url for davex connection
+ * jackrabbit-config, jackrabbit-home: local transport only. path to jackrabbit data folder
+
+Default values for all parameters are set in jcr.properties
 */
 public class Jack {
     public static void main(String[] args) throws Throwable {
-        if (args.length != 2) {
-            System.out.println("usage: java -jar impexp.jar (import|export) file.xml");
+        if (args.length < 2) {
+            System.out.println("usage: java -jar jack.jar (import|export) file.xml");
             System.exit(2);
         }
-        Jack j = new Jack();
+        Jack j = new Jack(args);
 
         int errorState = 0;
         try {
@@ -45,23 +56,33 @@ public class Jack {
     protected Repository repository;
     protected Session session;
     protected Properties config;
-    public Jack() throws Throwable {
+    public Jack(String[] args) throws Throwable {
         InputStream is = getClass().getClassLoader().getResourceAsStream("jcr.properties");
         if (is==null) throw new Exception("Could not find jcr.properties inside the jar");
         config = new Properties();
         config.load(is);
         is.close();
+        for (int i=2; i<args.length; i++) {
+            String pv[] = args[i].split("=",2);
+            if (pv.length != 2) throw new Exception("Invalid parameter "+args[i]);
+            config.setProperty(pv[0], pv[1]);
+        }
 
-        //network access (fails because of obscure logging library incompatibilites)
-        //client = new DavexClient(prop.getProperty("storage"));
-        //repository = client.getRepository();
+        String t = config.getProperty("transport");
+        if ("local".equals(t)) {
+            repository = new TransientRepository(config.getProperty("jackrabbit-config"),
+                                                    config.getProperty("jackrabbit-home"));
+        } else if ("davex".equals(t)) {
+            //network access (fails because of obscure logging library incompatibilites)
+            DavexClient client = new DavexClient(config.getProperty("storage"));
+            repository = client.getRepository();
+        } else {
+            throw new Exception("Unknown transport requested: "+config.getProperty("transport"));
+        }
 
-        //create local repository
-        repository = new TransientRepository(config.getProperty("jackrabbit-config"),
-                                             config.getProperty("jackrabbit-home"));
         SimpleCredentials cred = new SimpleCredentials(config.getProperty("username"),
                                                        config.getProperty("password").toCharArray());
-        session = repository.login(cred, config.getProperty("workspace", "default"));
+        session = repository.login(cred, config.getProperty("workspace"));
     }
     public void close() {
         session.logout();
